eb84ac3f7bb0ead30d0b27550ec43b11526b12a8
olorunwa-web <olorunwamoliki@gmail.com>
2025-07-11 17:52:16 +0100

    first-commit

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -99,7 +99,7 @@ const Navbar = () => {
     return (
         <>
             <nav className = "sticky w-full top-0 z-999 mt-1 border-b-1 border-[#F1F1F1]  navbar-contai ">
-                <div className = ' flex justify-between items-center px-4 md:px-[1.5rem] lg:px-[2rem] py-3 md:py-3 '>
+                <div className = ' flex justify-between items-center px-4 md:px-[1.5rem]  xl:px-[2rem] py-3 md:py-3 '>
                 <div className = 'flex items-center gap-1 hidden lg:flex'>
                     <img src= {handwave} className = 'w-6 h-6' alt=""/>
                     <div>
@@ -181,7 +181,7 @@ const Navbar = () => {
                    
                 </div>
                 <form className = "relative md:block hidden">
-                    <input type="search" name="" id="" value = {searchText} onChange={(e) => setSearchText(e.target.value)} placeholder = "Search" className = "w-full bg-[#F7F9FA] border-1 border-[#E0E0E0] py-2 px-4 rounded-sm outline-none appearance-none font-poppins font-normal text-base "/>
+                    <input type="search" name="" id="" value = {searchText} onChange={(e) => setSearchText(e.target.value)} placeholder = "Search" className = "w-ful xl:w-[320px] bg-[#F7F9FA] border-1 border-[#E0E0E0] py-2 pr-4 pl-4 xl:pr-8 xl:pl-5 rounded-sm outline-none appearance-none font-poppins font-normal text-base "/>
                     {!searchText && (
                       <div className = 'absolute right-3 top-1/2 transform -translate-y-1/2 w-5 h-5'>
                         <img src= {searchbtn} alt=""/>
